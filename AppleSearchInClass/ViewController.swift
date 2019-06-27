c0995903255c39e9712bc1f7982f744f567a87f0
Nic Gibson <nic@shpangy.com>
2019-06-27 10:12:22 -0600

    Initial Commit

---
+++ AppleSearchInClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleSearchInClass
+//
+//  Created by Nic Gibson on 6/27/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
