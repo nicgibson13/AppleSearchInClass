4bf597ef021c28c6975507cdf240338a89ae6259
Nic Gibson <nic@shpangy.com>
2019-06-27 10:30:57 -0600

    Laid out views

---
+++ AppleSearchInClass/UI/AppleItemTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  AppleItemTableViewCell.swift
+//  AppleSearchInClass
+//
+//  Created by Nic Gibson on 6/27/19.
+//  Copyright © 2019 Nic Gibson. All rights reserved.
+//
+
+import UIKit
+
+class AppleItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var trackLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var albumLabel: UILabel!
+    
+}
